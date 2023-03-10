1a7db050a8356ff0915aae63745a25f8cfe69969
th2ch-g <th2ch-g>
2023-03-10 12:22:57 +0900

    add

--- template/rust/base-atcoder.rs
+++ template/rust/base-atcoder.rs
@@ -1,14 +1,16 @@
 use proconio::{*};
 use proconio::marker::{*};
 use std::collections::{*};
+use petgraph::algo::{*};
+use petgraph::graph::{*};
+use petgraph::unionfind::{*};
+use petgraph::data::{*};
 #[fastout]
 fn main() {
     input! {
         n: usize,
     }
 
+    {{_cursor_}}
 
-    let mut answer = 0;
-
-    println!("{}", answer);
 }

66caf40e95b6c6fd34028ef0581c1496f4133188
th2ch-g <76892059+th2ch-g@users.noreply.github.com>
2023-06-09 16:01:35 +0900

    add

--- template/python/base-plot.py
+++ template/python/base-plot.py
@@ -2,14 +2,22 @@
 
 import os
 import sys
+import re
 import subprocess as sb
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
+from matplotlib.ticker import ScalarFormatter
 
 
+# seaborn theme
 sns.set(style = "darkgrid", palette = "muted", color_codes = True)
+
+# figure size
 fig, ax = plt.subplots()
 #fig, ax = plt.subplots(figsize=(19,13))
+
+# setting
 TITLE =
 XLABEL =
 YLABEL =
@@ -26,11 +34,17 @@ ax.set_xlabel(XLABEL)
 
 
 
+# # make x-axis 10^n
+# ax.set_xscale('linear')
+# ax.xaxis.set_major_formatter(ScalarFormatter(useMathText=True))
+# ax.xaxis.set_minor_formatter(ScalarFormatter(useMathText=True))
 
+# # x-axis label rotate
+# plt.xticks(rotation=60)
 
+# label left upper
+ax.legend(loc='upper left', bbox_to_anchor=(1, 1))
 
-plt.xticks(rotation=60)
-ax.grid()
-plt.grid()
+plt.grid(True)
 fig.tight_layout()
 plt.savefig(FIGNAME)

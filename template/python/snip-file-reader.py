8d8d7fc27bf77182bf66605e3f81d03218182c25
th2ch-g <76892059+th2ch-g@users.noreply.github.com>
2025-08-04 10:45:27 +0900

    add

--- template/python/snip-file-reader.py
+++ template/python/snip-file-reader.py
@@ -1,4 +1,4 @@
-with open({{_input_:file_name}}) as ref:
-    for idx, line in enumerate(ref):
+with open({{_input_:file_name}}) as f:
+    for idx, line in enumerate(f):
         line = line.rstrip()
         {{_cursor_}}
